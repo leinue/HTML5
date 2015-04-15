950111ea4ae84015455b981d24601d37c31343f9
ivy <597055914@qq.com>
2015-04-15 08:00:35 +0800

    update

--- 3DWeb/admin.php
+++ 3DWeb/admin.php
@@ -8,14 +8,64 @@
 <body>
 
     <header>
-        <nav></nav>
+        <nav>Virtual Musuem Administration Panel</nav>
     </header>
+
+    <section>
+        <div class="admin-menu">
+            <ul>
+                <li><span class="admin-menu-active">Manage Models</span></li>
+                <li><span>Manage Background</span></li>
+            </ul>
+        </div>
+
+        <div id="list-panel" class="operate-panel">
+        <ul>
+        <?php 
+        function listDir($dir){
+            if(is_dir($dir)){
+                if ($dh = opendir($dir)) {
+                    while (($file = readdir($dh)) !== false){
+                        if((is_dir($dir."/".$file)) && $file!="." && $file!=".."){
+                            echo "<li id=\"\">model/$file<li>";
+                            listDir($dir."/".$file."/");
+                        }
+                        else{
+                            if($file!="." && $file!=".."){
+                                $ext=explode(".",$file);
+                                if($ext[count($ext)-1]!='x3d'){
+                                    $id=$ext[0];
+                                    echo "<li id=\"$id\"><img src=\"model/$file\"><li>";
+                                    $id='';
+                                }
+                            }
+                        }
+                    }
+                    closedir($dh);
+                }
+            }
+        }
+        //开始运行
+        listDir("model");
+        ?>
+        </ul>
+    </div>
+    </section>
     
     <footer>
         <div>
             <p>Virtual Musuem Built by jQuery & X3DOM & CSS3</p>
         </div>
     </footer>
-
+    <script type="text/javascript">
+        $('a,h1,h2,h3,h4,h5,h6,p,span').hover(
+            function(){
+                $(this).stop().animate({opacity:0.6},'fast');
+            },
+            function(){
+                $(this).stop().animate({opacity:1},'fast');
+            }
+        );
+    </script>
 </body>
 </html>
